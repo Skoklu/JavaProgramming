4daa13f3e257a185374f8fc7b15f3a66c3e6dd5a
selma <sunal.32@hotmail.com>
2022-04-29 11:31:02 +0100

    day43: Abstract class & Abstract method_Employee

---
+++ src/day43_Abstraction/employeeTask/Person.java
@@ -0,0 +1,69 @@
+package day43_Abstraction.employeeTask;
+
+public abstract class Person {
+
+    private String name;
+    private int age;
+    private char gender;
+
+
+    //Constructor
+    public Person(String name, int age, char gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    //3 getters and 3 setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null){
+            throw new RuntimeException("Invalid entry");
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age <0){
+            throw new RuntimeException(age + " is invalid enter");
+        }
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        if (!(gender=='m' || gender =='F')){
+            throw new RuntimeException(gender + " is invalid entry");
+        }
+        this.gender = gender;
+    }
+
+    //Abstract Method
+    public abstract void sleep();
+
+
+    //Instance Method
+    public void eat(){
+        System.out.println(name+ "is eating baklava");
+    }
+
+
+    //to String Method
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+}
