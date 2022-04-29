1334695210b2b981ebc3925e25d46dfae86b40cb
selma <sunal.32@hotmail.com>
2022-04-29 12:16:02 +0100

    day43: Abstract class & Abstract method_Employee

---
+++ src/day43_Abstraction/employeeTask/EmployeeObjects.java
@@ -0,0 +1,22 @@
+package day43_Abstraction.employeeTask;
+
+public class EmployeeObjects {
+
+    public static void main(String[] args) {
+
+        //We don't create an object class from Person and Employee, because they are abstraction class
+        //Person person1 = new Person("Josh", 35, 'M');
+       // Employee employee = new Employee("Ali", 30, 'M', "SDET", 115000.00)//
+
+        Tester tester1 = new Tester("Ali", 30, 'M', 42, 100000);
+        Developer developer1 = new Developer("Alex", 28, 'M',32, "Developer",125000);
+        Teacher teacher1 =new Teacher("Angal", 34, 'F', 52, "Math Teacher", 85000);
+        Driver driver1 = new Driver("Jhon", 43,'M', 62,"Lyft Driver", 75000.00);
+
+
+        System.out.println(tester1);
+        System.out.println(developer1);
+        System.out.println(teacher1);
+        System.out.println(driver1);
+    }
+}
