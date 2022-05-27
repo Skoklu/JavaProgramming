7d2e7649b210f17bb9a239e0dd15b030631f6486
selma <sunal.32@hotmail.com>
2022-05-27 16:27:24 +0100

    day49_Set Interface Practice

--- src/day49_Collections/SetInterfacePractice.java
+++ src/day49_Collections/SetInterfacePractice.java
@@ -13,7 +13,8 @@ public class SetInterfacePractice {
         list.addAll(Arrays.asList(10, 200, 300, 40, 90));
         list.addAll(Arrays.asList(10, 200, 300, 40, 90));
         list.addAll(Arrays.asList(null, null, null));
-        System.out.println(list);
+        System.out.println("list = " + list);
+        System.out.println(list.get(4));
 
         Set<Integer> hashSet = new HashSet<>();
         hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
@@ -21,10 +22,32 @@ public class SetInterfacePractice {
         hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
         hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
         hashSet.addAll(Arrays.asList(null, null, null));
-        System.out.println(hashSet);
+        System.out.println("hashSet = " + hashSet);
+        //System.out.println(hashSet.get(4));
+
 
         Set<Integer> linkedHashSet = new LinkedHashSet<>();
+        
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(null, null, null));
+
+        System.out.println("linkedHashSet = " + linkedHashSet);
+
 
         Set<Integer> treeSet = new TreeSet<>();
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        //treeSet.addAll(Arrays.asList(null, null, null));  //treeSet not accept null element
+        System.out.println("treeSet = " + treeSet);
+        String str = null;
+
+        System.out.println(str.charAt(0));
+
     }
 }
