c281d1a264d4d490e1ab2cc83ee8ca8feef149f5
selma <sunal.32@hotmail.com>
2022-05-28 22:24:28 +0100

    day49: Collections continue(Queue practice)

---
+++ src/day49_Collections/QueuePractice.java
@@ -0,0 +1,54 @@
+package day49_Collections;
+
+import java.util.*;
+
+public class QueuePractice {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        System.out.println("priorityQueue = " + priorityQueue);
+
+        Queue<Integer> arrayDeque = new PriorityQueue<>();
+        arrayDeque.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        System.out.println("arrayQueue = " + arrayDeque);
+
+        Queue<Integer> linkedList = new PriorityQueue<>();
+        linkedList.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        System.out.println("linkedList = " + linkedList);
+
+        System.out.println("------------------------------------------------------");
+
+        priorityQueue.poll();
+        System.out.println(priorityQueue);
+
+
+        priorityQueue.poll();
+        System.out.println(priorityQueue);
+
+        arrayDeque.poll();
+        System.out.println(arrayDeque);
+
+        linkedList.poll();
+        System.out.println(linkedList);
+
+        //System.out.println(priorityQueue.get(1));
+
+        //System.out.println(arrayDeque.get(1));
+
+        System.out.println(((List)linkedList).get(1));
+                    //OR
+        //System.out.println(((LinkedList)linkedList).get(1));
+                    //OR
+        System.out.println(((LinkedList<Integer>)linkedList).get(1));
+
+
+
+
+
+    }
+}
