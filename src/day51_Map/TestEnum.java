e1a00b96c879c29e56ac00c575946deb700ca696
selma <sunal.32@hotmail.com>
2022-06-01 16:43:49 +0100

    day51: Test ENUM

--- src/day51_Map/TestEnum.java
+++ src/day51_Map/TestEnum.java
@@ -133,5 +133,37 @@ public class TestEnum {
                 System.out.println("Month is December");
         }
 
+        Day day = Day.FRIDAY;
+
+        switch (day){
+
+            case MONDAY:
+                System.out.println(" Today is Monday");
+                break;
+
+            case TUESDAY:
+                System.out.println(" Today is Tuesday");
+                break;
+
+            case WEDNESDAY:
+                System.out.println(" Today is Wednesday");
+                break;
+
+            case THURSDAY:
+                System.out.println(" Today is Thursday");
+                break;
+
+            case FRIDAY:
+                System.out.println(" Today is Friday");
+                break;
+
+            case SATURDAY:
+                System.out.println(" Today is Saturday");
+                break;
+
+            default:
+                System.out.println("Today is Sunday");
+        }
+
     }
 }
